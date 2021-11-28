0279da668e29e46d16e1ff1ad0383a2d4515db78
Ben <w.ben.ballard@gmail.com>
2021-11-28 11:01:22 -0500

    commit on 'Main

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,17 @@ import Header from '@components/Header'
 export default function Home() {
   return (
     <div className="container">
-      <Head>
+      <Head class="post-header">
         <title>Ben Ballard</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome" />
-
+        <article class="post-content">
+          <h3 id="about-me">About Me</h3>
+          <p>Ben Ballard is a cybersecurity engineer at the MITRE Corporation and an affiliate of the Citizen Lab at the University of Toronto. Ben has served as a Google Public Policy Fellow at the Electronic Frontier Foundation and an X-Force Fellow with the National Security Innovation Network. His research centers around the international governance of information security and privacy, especially data appropriation, surveillance, and deception. At work, he contributes to hardware security projects and privacy attacks research.</p>
+        </article>
       </main>
 
     </div>
