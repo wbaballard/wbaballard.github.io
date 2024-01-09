8e226ddd1d6fbc104ef3ef97431e761476bf6d6b
Ben Ballard <32252244+wbaballard@users.noreply.github.com>
2024-01-09 11:24:16 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome" />
           <p className="description">
-            Ben Ballard is a cybersecurity engineer at the MITRE Corporation. Ben has served as a Google Public Policy Fellow at the Electronic Frontier Foundation and an X-Force Fellow with the National Security Innovation Network. His research centers around the international governance of information security and privacy, especially data appropriation, surveillance, and deception. At work, he contributes to hardware security projects and privacy attacks research.
+            Ben Ballard is a cybersecurity engineer at the MITRE Corporation. Ben has served as a Google Public Policy Fellow at the Electronic Frontier Foundation, an X-Force Fellow with the National Security Innovation Network, and a cybersecurity fellow at the Citizen Lab at the Munk School of Global Affairs & Public Policy, University of Toronto. His research centers around the international governance of information security and privacy, especially data appropriation, surveillance, and deception. At work, he supports hardware security projects, cyber resilience efforts, risk assessments, international capacity building, and privacy attacks research.
           </p>
       </main>
 
